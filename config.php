ce77f9c91d4a7e976add90c84d1f2e7546429e61
Vartic Mihai-Vlad <61534490+Varothex@users.noreply.github.com>
2020-11-19 17:00:49 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,5 @@
 <?php
-/*define('DB_SERVER', 'sql206.byetcluster.com');
-define('DB_USERNAME', 'epiz_27117785');
-define('DB_PASSWORD', 'fiCNhIXlBS0cZ');
-define('DB_NAME', 'epiz_27117785_doctor_doom');*/
-
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'doctor doom');
- 
+//detalii cont
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 if($link === false){
