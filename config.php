15964aacd766c629a908f72157b6e7cc99b679c3
Vartic Mihai-Vlad <61534490+Varothex@users.noreply.github.com>
2021-01-14 15:23:06 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
-define('DB_SERVER', 'sql206.epizy.com');
-define('DB_USERNAME', 'epiz_27117785');
-define('DB_PASSWORD', 'fiCNhIXlBS0cZ');
-define('DB_NAME', 'epiz_27117785_doctor_doom');
+define('DB_SERVER', '***');
+define('DB_USERNAME', '***');
+define('DB_PASSWORD', '***');
+define('DB_NAME', '***');
 
 /*define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'doctor doom');*/
+define('DB_NAME', '***');*/
  
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
